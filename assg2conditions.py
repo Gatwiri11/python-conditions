70cfc2da914f726c8b75548bac31aba6960c1b36
unknown <emmahgatwir38@gmail.com>
2023-07-03 13:10:39 -0700

    pyconditions

---
+++ assg2conditions.py
@@ -0,0 +1,34 @@
+marks = float(input("enter marks:"))
+
+if marks > 88:
+    print("A")
+
+elif marks >= 80 and marks <88:
+    print("A-")
+
+elif marks >=73 and marks < 80:
+    print("B+")
+
+elif marks >=66 and marks < 73:
+    print("B")
+
+elif marks >=60 and marks < 66:
+    print("B-")
+
+elif marks >=55 and marks < 60:
+    print("C+")
+
+elif marks >=50 and marks <55:
+     print("C")
+
+elif marks >=45 and marks <50:
+     print("C-")
+
+elif marks >=40 and marks <45:
+     print("D")
+
+else:print("F")
+
+
+
+
